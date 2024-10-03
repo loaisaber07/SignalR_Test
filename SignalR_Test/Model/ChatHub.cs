ee4a56940c203f0ff3977a18de5907d2e8eb3b2c
loaisaber07 <loai.sabir@outlook.com>
2024-10-03 04:08:54 +0300

    Allow Chat fro Group of People

---
+++ SignalR_Test/Model/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace SignalR_Test.Model
+{
+    public class ChatHub : Hub
+    {
+        public void sendMessage(string userName, string message)
+        { 
+        //save it into your own DataBase
+    Clients.All.SendAsync("ReceiveMessage", userName, message);
+        
+        }
+    }
+}
--- SignalR_Test/Program.cs
+++ SignalR_Test/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.Extensions.Options;
+using SignalR_Test.Model;
+
 namespace SignalR_Test
 {
     public class Program
@@ -13,24 +16,37 @@ namespace SignalR_Test
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            builder.Services.AddSignalR();
+            builder.Services.AddCors(op =>
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                op.AddDefaultPolicy(policy =>
+                {
+
+                    policy.WithOrigins("http://localhost:4200") // Allow Angular's local development URL
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+
+                });
+            });
+                var app = builder.Build();
+
+                app.UseCors();
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.MapHub<ChatHub>("/chatHub");
+                app.MapControllers();
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
     }
-}
+    } 
